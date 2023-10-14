c233f9a9dfbcca038a968c1598f2bd137e228fa5
erenlifetime <medya_194@hotmail.com>
2023-10-14 19:34:21 +0300

    Initial Commit

---
+++ MyPersonelCard/MyPersonelCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPersonelCardApp.swift
+//  MyPersonelCard
+//
+//  Created by Eren lifetime on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyPersonelCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
